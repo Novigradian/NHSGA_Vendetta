a5a017da7c07724cf3ec66b61c002cd88ad963e6
LucasWang <100511265+Novigradian@users.noreply.github.com>
2023-08-08 01:39:10 -0400

    lighting for main menu

---
+++ Assets/Scripts/UI Scripts/ChangeButtonColor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ChangeButtonColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private TMP_Text buttonText;
+
+    private Color originalColor;
+
+    public Color hoverColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        originalColor = buttonText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.color = hoverColor;
+        Debug.Log("enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.color = originalColor;
+        Debug.Log("exit");
+    }
+}
