c8a4c845e95ba1c2d8845e4afc6b5b4d2f4515fc
cchang3906 <cchang3906@gmail.com>
2023-08-08 13:29:35 -0400

    boss animation + rally fix

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -1124,6 +1124,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(rallyDuration);
         isRallyOn = false;
+        rallyAnimator.Play("Idle");
     }
 
     private void ActivateRally()
